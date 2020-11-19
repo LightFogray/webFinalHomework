baa402fcccd402be8274002a475ac3c53f1df318
SmallMoonHalf <49306778+SmallMoonHalf@users.noreply.github.com>
2020-11-19 19:33:13 +0800

    Update message.php

--- message.php
+++ message.php
@@ -6,7 +6,7 @@ session_start();
 // D:\xampp\php
 $host = '127.0.0.1';
 $dbuser = 'root';
-$pwd = 'qwh263625';
+$pwd = '123456';
 $dbname = 'qian';	//数据库名
 $db = new mysqli( $host, $dbuser, $pwd, $dbname);
 
